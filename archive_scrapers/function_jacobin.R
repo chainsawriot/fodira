d47b31967e4e855308a50ba1eeb4288f7c8d9f70
Rainer Freudenthaler <rainer.freudenthaler@uni-mannheim.de>
2022-09-08 11:25:27 +0200

    change date lines in twitter scrapers - sys.time didn't work

--- archive_scrapers/function_jacobin.R
+++ archive_scrapers/function_jacobin.R
@@ -2,14 +2,15 @@ require(academictwitteR)
 require(dplyr)
 
 dir <- academictwitteR:::.gen_random_dir()
-x <- academictwitteR:::get_all_tweets("from:jacobinmag_de", start_tweets = "2021-12-31T00:00:00Z", n = Inf, bind_tweets = FALSE, data_path = dir, is_retweet = FALSE)
+x <- academictwitteR:::get_all_tweets("from:jacobinmag_de", start_tweets = "2021-12-31T00:00:00Z", end_tweets = "2022-09-02T00:00:00Z", n = Inf, bind_tweets = FALSE, data_path = dir, is_retweet = FALSE)
 
 res <- academictwitteR:::bind_tweets(dir, output_format = "tidy")
 all_urls <- stringr::str_extract_all(res$text, "https://t.co/[a-zA-Z0-9]+")
 
 
-.resolve <- function(tcourl) {
-    httr::HEAD(tcourl)$url
+.resolve <- function(tcourl){
+  cat(tcourl)
+  httr::HEAD(tcourl)$url
 }
 
 res$resolved_urls <- purrr::map(all_urls, ~purrr::map_chr(., .resolve))
@@ -25,4 +26,8 @@ res$resolved_urls <- purrr::map(all_urls, ~purrr::map_chr(., .resolve))
 
 res$url <- res$resolved_urls %>% purrr::map_chr(.sub)
 
-res %>% select(created_at, text, url) %>% rename(title = `text`, link = url, pubdate = created_at) %>% mutate(pubdate = lubridate::ymd_hms(pubdate)) %>% mutate(pub = "Jacobin", description = NA) %>% arrange(pubdate) %>% select(pub, link, pubdate, title, description) %>% saveRDS("jacobin_archive.RDS")
+res %>% select(created_at, text, url) %>% rename(title = `text`, link = url, pubdate = created_at) %>% 
+  mutate(pubdate = lubridate::ymd_hms(pubdate)) %>% mutate(pub = "Jacobin", description = NA) %>% 
+  arrange(pubdate) %>% select(pub, link, pubdate, title, description) -> valid_links
+  
+saveRDS(valid_links, "jacobin_archive.RDS")

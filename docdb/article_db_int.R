e68787b9cad3b21af7dd66c142eeacaedf47aa7d
chainsawriot <chainsawtiney@gmail.com>
2022-09-09 13:34:45 +0200

    Disable the two indices
    Which are super big.

--- docdb/article_db_int.R
+++ docdb/article_db_int.R
@@ -12,8 +12,8 @@ if (file.exists(Sys.getenv('ARTICLE_DB'))) {
 con <- DBI::dbConnect(duckdb::duckdb(), dbdir = Sys.getenv('ARTICLE_DB'), read_only = FALSE)
 DBI::dbExecute(con, "CREATE SEQUENCE serial START 1;")
 DBI::dbExecute(con, paste(readLines("create_table.sql"), collapse = ""))
-DBI::dbExecute(con, "CREATE INDEX link_index ON articles (link);")
+#DBI::dbExecute(con, "CREATE INDEX link_index ON articles (link);")
 DBI::dbExecute(con, "CREATE INDEX pub_index ON articles (pub);")
-DBI::dbExecute(con, "CREATE INDEX fl_link_index ON failedurls (link);")
+#DBI::dbExecute(con, "CREATE INDEX fl_link_index ON failedurls (link);")
 DBI::dbDisconnect(con, shutdown = TRUE)
 

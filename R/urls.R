471222b322a1d3d6fd8d6c32819510473fa35d9b
chainsawriot <chainsawtiney@gmail.com>
2022-09-18 23:20:56 +0200

    Add documentation

--- R/urls.R
+++ R/urls.R
@@ -1,3 +1,11 @@
+#' Modify urls
+#'
+#' Modify urls, either by removing the query part, or the domain names.
+#' @param url character, input url
+#' @param rm_query logical, whether to remove the query part
+#' @param replace_domains character vector, when is not NULL, one domain from this vector is chosen to replace the hostname of the input url
+#' @return a modified url
+#' @author Chung-hong Chan
 #' @export
 modify_url <- function(url, rm_query = TRUE, replace_domains = NULL) {
     parsed_url <- httr::parse_url(url)

b725041f98b8bafa2bda4bf258f92956120ad76e
chainsawriot <chainsawtiney@gmail.com>
2022-10-06 18:30:02 +0200

    Add dispencer for test urls

---
+++ page_dl/get_test_link.R
@@ -0,0 +1,15 @@
+#!/usr/bin/env Rscript
+
+library(fodira)
+args <- commandArgs(trailingOnly=TRUE)
+
+.get_urls <- function(con, safe) {
+    if (safe) {
+        con$aggregate('[ {"$match": {"htmlfile": "", "pub" : { "$in": ["Bild", "Tagesschau", "Heute", "Freitag", "T-Online"]}}}, { "$sample": { "size": 300} }]')$link
+    } else {
+        con$aggregate('[ {"$match": {"htmlfile": "", "pub": { "$nin": ["Zeit"]}}}, { "$sample": { "size": 300} }]')$link
+    }
+}
+
+con <- mongolite::mongo("articles", db = "main")
+saveRDS(.get_urls(con, safe = TRUE), args[1])

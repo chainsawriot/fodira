a98685d48d492a764bb1f9bc19147bda3607f2c9
chainsawriot <chainsawtiney@gmail.com>
2022-10-07 13:20:02 +0200

    Add make the test work even w/o MongoDB, simplify page_dl, add worker_dl

--- tests/testthat/test_insert_doc_link.R
+++ tests/testthat/test_insert_doc_link.R
@@ -1,5 +1,10 @@
 test_that("test_insert", {
     skip_on_ci()
+    tryCatch({
+        con <- mongolite::mongo(collection = "articles", db = "main", verbose = FALSE)
+    }, error = function(e) {
+        skip("No MongoDB")
+    })
     testdata <- tibble::tibble(pub = "a", link = "a", pubdate = NA)
     expect_error({res <- insert_doc_link(testdata, db = "main", collection = "testthat")}, NA) # no error
     expect_true(nrow(res) == 1)
